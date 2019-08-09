e5bbdcdb4aaf2d4329e7df0446f0557b69f839f6
4rfel <38050598+4rfel@users.noreply.github.com>
2019-08-09 16:15:58 -0300

    aa

---
+++ aplicacao.py
@@ -0,0 +1,116 @@
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#####################################################
+# Camada Física da Computação
+#Carareto
+#17/02/2018
+#  Aplicação 
+####################################################
+
+print("comecou")
+
+from enlace import *
+import time
+
+import cv2
+
+
+print("OpenCV Version : %s " % cv2.__version__)
+
+cap = cv2.VideoCapture(0)
+
+cap.set(cv2.CAP_PROP_FRAME_WIDTH,320/2)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240/2)
+
+ret, frame = cap.read()
+
+cap.release()
+
+cv2.imwrite("foto_original.png", frame)
+
+
+
+# Serial Com Port
+#   para saber a sua porta, execute no terminal :
+#   python -m serial.tools.list_ports
+
+#serialName = "/dev/ttyACM0"           # Ubuntu (variacao de)
+#serialName = "/dev/tty.usbmodem1411" # Mac    (variacao de)
+serialName = "COM3"                  # Windows(variacao de)
+print("abriu com")
+
+def main():
+    # Inicializa enlace ... variavel com possui todos os metodos e propriedades do enlace, que funciona em threading
+    com = enlace(serialName) # repare que o metodo construtor recebe um string (nome)
+    # Ativa comunicacao
+    com.enable()
+
+   
+
+    # Log
+    print("-------------------------")
+    print("Comunicação inicializada")
+    print("  porta : {}".format(com.fisica.name))
+    print("-------------------------")
+
+    # Carrega dados
+    print ("gerando dados para transmissao :")
+  
+      #no exemplo estamos gerando uma lista de bytes ou dois bytes concatenados
+    
+    #exemplo 1
+    #ListTxBuffer =list()
+    #for x in range(1,10):
+    #    ListTxBuffer.append(x)
+    #txBuffer = bytes(ListTxBuffer)
+    
+    #exemplo2
+
+    # txBuffer = bytes([2]) + bytes([3])+ bytes("teste", 'utf-8')
+    with open("foto_original.png", "rb") as foto:
+      txBuffer = foto.read()
+    
+    txLen    = len(txBuffer)
+    print(txLen)
+
+    # Transmite dado
+    print("tentado transmitir .... {} bytes".format(txLen))
+    com.sendData(txBuffer)
+
+    # espera o fim da transmissão
+    #while(com.tx.getIsBussy()):
+    #    pass
+    
+    
+    # Atualiza dados da transmissão
+    txSize = com.tx.getStatus()
+    print ("Transmitido       {} bytes ".format(txSize))
+
+    # Faz a recepção dos dados
+    print ("Recebendo dados .... ")
+    
+    #repare que o tamanho da mensagem a ser lida é conhecida!     
+    rxBuffer, nRx = com.getData(txLen)
+
+    f = open("foto_copiada.jpg", "wb")
+    f.write(rxBuffer)
+    f.close()
+    
+
+    # log
+    print ("Lido              {} bytes ".format(nRx))
+    
+    print (rxBuffer)
+
+    
+
+    # Encerra comunicação
+    print("-------------------------")
+    print("Comunicação encerrada")
+    print("-------------------------")
+    com.disable()
+
+    #so roda o main quando for executado do terminal ... se for chamado dentro de outro modulo nao roda
+if __name__ == "__main__":
+    main()
