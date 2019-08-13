0ca2e1037238de39a03550e0bc0b66e132686215
4rfel <38050598+4rfel@users.noreply.github.com>
2019-08-13 19:14:23 -0300

    aa

--- aplicacao_Receptor.py
+++ aplicacao_Receptor.py
@@ -22,64 +22,21 @@ serialName = "COM4"                  # Windows(variacao de)
 
 
 def main():
-    # Inicializa enlace ... variavel com possui todos os metodos e propriedades do enlace, que funciona em threading
     com = enlace(serialName) # repare que o metodo construtor recebe um string (nome)
     # Ativa comunicacao
     com.enable()
 
-    # Log
-    # print("-------------------------")
     print("Comunicação inicializada")
-    # print("  porta : {}".format(com.fisica.name))
-    # print("-------------------------")
-
-    # # Carrega dados
-    # print ("gerando dados para transmissao :")
-  
-      #no exemplo estamos gerando uma lista de bytes ou dois bytes concatenados
-      
-    #exemplo 1
-    #ListTxBuffer =list()
-    #for x in range(1,10):
-    #    ListTxBuffer.append(x)
-    #txBuffer = bytes(ListTxBuffer)
-    
-    #exemplo2
-    # txBuffer = bytes([2]) + bytes([3])+ bytes("teste", 'utf-8')
     
     
-    # txLen    = len(txBuffer)
-    # print(txLen)
-
-    # Transmite dado
-    # print("tentado transmitir .... {} bytes".format(txLen))
-    # com.sendData(txBuffer)
-
-    # espera o fim da transmissão
-    # while(com.tx.getIsBussy()):
-    #    pass
-    
-    
-    # Atualiza dados da transmissão
     txSize = com.tx.getStatus()
-    # print ("Transmitido       {} bytes ".format(txSize))
-
-    # # Faz a recepção dos dados
-    # print ("Recebendo dados .... ")
-    
-    #repare que o tamanho da mensagem a ser lida é conhecida! 
 
     txLen, nRxLen = com.getData(2)
     txLen = int.from_bytes(txLen, 'big')
 
-    # print("txLen é             :" + str(txLen))
 
     rxBuffer, nRx = com.getData(txLen) 
-
-    # log
-    # print ("Lido              {} bytes ".format(nRx))
     
-    # print (rxBuffer)
     com.sendData(nRx.to_bytes(2, "big"))
 
 
