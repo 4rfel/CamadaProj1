da1fa8194802d0d9ac585d6f587a31c8dd024457
4rfel <38050598+4rfel@users.noreply.github.com>
2019-08-13 18:55:26 -0300

    aa

--- aplicacao_Receptor.py
+++ aplicacao_Receptor.py
@@ -8,13 +8,8 @@
 #  Aplicação 
 ####################################################
 
-print("comecou")
-
-from enlace_recebe import *
-import time
+from enlace import *
 import binascii
-
-
 
 # Serial Com Port
 #   para saber a sua porta, execute no terminal :
@@ -22,9 +17,8 @@ import binascii
 
 #serialName = "/dev/ttyACM0"           # Ubuntu (variacao de)
 #serialName = "/dev/tty.usbmodem1411" # Mac    (variacao de)
-serialName = "COM5"                  # Windows(variacao de)
+serialName = "COM4"                  # Windows(variacao de)
 
-print("abriu com")
 
 def main():
     # Inicializa enlace ... variavel com possui todos os metodos e propriedades do enlace, que funciona em threading
@@ -33,13 +27,13 @@ def main():
     com.enable()
 
     # Log
-    print("-------------------------")
+    # print("-------------------------")
     print("Comunicação inicializada")
-    print("  porta : {}".format(com.fisica.name))
-    print("-------------------------")
+    # print("  porta : {}".format(com.fisica.name))
+    # print("-------------------------")
 
-    # Carrega dados
-    print ("gerando dados para transmissao :")
+    # # Carrega dados
+    # print ("gerando dados para transmissao :")
   
       #no exemplo estamos gerando uma lista de bytes ou dois bytes concatenados
       
@@ -50,35 +44,45 @@ def main():
     #txBuffer = bytes(ListTxBuffer)
     
     #exemplo2
-    txBuffer = bytes([2]) + bytes([3])+ bytes("teste", 'utf-8')
+    # txBuffer = bytes([2]) + bytes([3])+ bytes("teste", 'utf-8')
     
     
-    txLen    = len(txBuffer)
-    print(txLen)
+    # txLen    = len(txBuffer)
+    # print(txLen)
 
     # Transmite dado
-    print("tentado transmitir .... {} bytes".format(txLen))
-    com.sendData(txBuffer)
+    # print("tentado transmitir .... {} bytes".format(txLen))
+    # com.sendData(txBuffer)
 
     # espera o fim da transmissão
-    #while(com.tx.getIsBussy()):
+    # while(com.tx.getIsBussy()):
     #    pass
     
     
     # Atualiza dados da transmissão
     txSize = com.tx.getStatus()
-    print ("Transmitido       {} bytes ".format(txSize))
+    # print ("Transmitido       {} bytes ".format(txSize))
 
-    # Faz a recepção dos dados
-    print ("Recebendo dados .... ")
+    # # Faz a recepção dos dados
+    # print ("Recebendo dados .... ")
     
     #repare que o tamanho da mensagem a ser lida é conhecida! 
-    rxBuffer, nRx = com.getData(int(binascii.unhexlify(txLen)))
+    txLen, nRxLen = com.getData(2)
+    txLen = int.from_bytes(txLen, 'big')
+
+    # print("txLen é             :" + str(txLen))
+
+    rxBuffer, nRx = com.getData(txLen) 
 
     # log
-    print ("Lido              {} bytes ".format(nRx))
+    # print ("Lido              {} bytes ".format(nRx))
     
-    print (rxBuffer)
+    # print (rxBuffer)
+    com.sendData(nRx.to_bytes(2, "big"))
+
+
+    with open("received_image" + str(testNumber) + ".png", "wb") as image:
+        image.write(rxBuffer)
 
     
 
@@ -89,5 +93,9 @@ def main():
     com.disable()
 
     #so roda o main quando for executado do terminal ... se for chamado dentro de outro modulo nao roda
+testNumber = 0
 if __name__ == "__main__":
+    # for e in range(10):
+        # main()
+        # testNumber += 1
     main()
